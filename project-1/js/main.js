e411c242a58d734986710c486095303dad8f0cdc
Sean Ross <seanmross9@gmail.com>
2020-01-28 13:02:28 -0500

    created transitions between data changes

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -11,6 +11,12 @@ const group = {
     height: canvas.height - margin.top - margin.bottom
 }
 
+// Show 'revenue' data if true, else show 'profit' data
+let flag = true;
+
+// Smooth transition switching between 'revenue' and 'profit' data
+let t = d3.transition().duration(750)
+
 // Canvas
 let svg = d3.select('#chart-area')
     .append('svg')
@@ -22,13 +28,13 @@ let g = svg.append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`)
 
 // x-scale
-let scaleX = d3.scaleBand()
+let xScale = d3.scaleBand()
     .range([0, group.width])
     .paddingInner(0.3)
     .paddingOuter(0.3)
 
 // y-scale
-let scaleY = d3.scaleLinear()
+let yScale = d3.scaleLinear()
     .range([group.height, 0])
 
 // x-axis group
@@ -48,7 +54,7 @@ g.append('text')
     .text('Month')
 
 // y-axis label
-g.append('text')
+let yLabel = g.append('text')
     .attr('x', -(group.height / 2))
     .attr('y', -(margin.left / 2 + 20))
     .attr('transform', 'rotate(-90)')
@@ -57,50 +63,64 @@ g.append('text')
 
 d3.json('../data/revenues.json').then( data => {
     // Transform revenue data
-    data.forEach(d => d.revenue = +d.revenue)
+    data.forEach(d => {
+        d.revenue = +d.revenue;
+        d.profit = +d.profit;
+    })
 
     d3.interval( () => {
-        update(data);
-        console.log('update')
+        let newData = flag ? data : data.slice(1)
+        update(newData)
+        flag = !flag
     }, 1000)
 
     update(data)
 })
 
 function update(data) {
+    let val = flag ? 'revenue' : 'profit'
+    
     // update domains
-    scaleX.domain(data.map(d => d.month))
-    scaleY.domain([0, d3.max(data, d => d.revenue)])
+    xScale.domain(data.map(d => d.month))
+    yScale.domain([0, d3.max(data, d => d[val])])
 
     // update axes
-    let xAxis = d3.axisBottom(scaleX)
-    xAxisGroup.call(xAxis)
+    let xAxis = d3.axisBottom(xScale)
+    xAxisGroup.transition(t).call(xAxis)
 
-    let yAxis = d3.axisLeft(scaleY)
+    let yAxis = d3.axisLeft(yScale)
         .tickFormat(d => `$ ${d.toLocaleString()}`)
-    yAxisGroup.call(yAxis)
+    yAxisGroup.transition(t).call(yAxis)
+    let label = flag ? 'Revenue' : 'Profit'
+    yLabel.transition(t).text(label)
 
     // JOIN new data with old elements
     let rects = g.selectAll('rect')
-        .data(data)
+        .data(data, d => d.month)
     
     // EXIT (remove) old elements not present in new data
-    rects.exit().remove()
-
-    // UPDATE old elements present in new data
-    rects.attr('x', d => scaleX(d.month))
-        .attr('y', d => scaleY(d.revenue))
-        .attr('width', scaleX.bandwidth)
-        .attr('height', d => group.height - scaleY(d.revenue))
-    
+    rects.exit()
+        .attr('fill', 'red')
+        .transition(t)
+            .attr('y', yScale(0))
+            .attr('height', 0)
+            .remove()
+        
     // ENTER new elements present in new data
     rects.enter()
         .append('rect')
-        .attr('x', d => scaleX(d.month))
-        .attr('y', d => scaleY(d.revenue))
-        .attr('width', scaleX.bandwidth)
-        .attr('height', d => group.height - scaleY(d.revenue))
-        .attr('fill', 'orange')
+        .attr('fill', 'grey')
+        .attr('y', yScale(0))
+        .attr('height', 0)
+        .attr('x', d => xScale(d.month))
+        .attr('width', xScale.bandwidth)
+        // AND UPDATE old elements present in new data
+        .merge(rects)
+        .transition(t)
+            .attr('x', d => xScale(d.month))
+            .attr('y', d => yScale(d[val]))
+            .attr('width', xScale.bandwidth)
+            .attr('height', d => group.height - yScale(d[val]))
 
     console.log(rects)
 
