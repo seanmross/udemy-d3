b577fdb9f666f33afdb17fa5a038bb02420d63e7
Sean Ross <seanross917@gmail.com>
2020-02-03 08:20:03 -0500

    created axis, labels, and populated scatter plot with first year data

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -11,40 +11,55 @@ const plot = {
 	width: canvas.width - margin.left - margin.right
 }
 
-// SVG canvas element
-let svg = d3.select('#chart-area')
+// SVG canvas and visualization group elements
+let g = d3.select('#chart-area')
 	.append('svg')
-	.attr('height', canvas.height)
-	.attr('width', canvas.width)
+		.attr('height', canvas.height)
+		.attr('width', canvas.width)
+	.append('g')
+		.attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-// Visualization group (plot area)
-let vis = svg.append('g')
-	.attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-// X-scale
-let xScale = d3.scaleLog()
+// Scales
+let x = d3.scaleLog()
+	.base(10)	
 	.domain([300, 150000])
 	.range([0, plot.width])
-	.base(10)
-
-// Y-scale
-let yScale = d3.scaleLinear()
+let y = d3.scaleLinear()
 	.domain([0,90])
-	.range([0, plot.height])
+	.range([plot.height, 0])
 
-// X-axis
-let xAxis = d3.axisBottom(xScale)
+// Axis
+let xAxis = d3.axisBottom(x)
 	.tickValues([400, 4000, 40000])
-vis.append('g')
+	.tickFormat(d => `$ ${d.toLocaleString()}`) // Have to use tickFormat() when using log scale (if tick values are not base 10)
+g.append('g')
 	.attr('id', 'x-axis')
 	.attr('transform', `translate(0, ${plot.height})`)
 	.call(xAxis)
-
-// Y-axis
-let yAxis = d3.axisLeft(yScale)
-vis.append('g')
+let yAxis = d3.axisLeft(y)
+g.append('g')
 	.attr('id', 'y-axis')
 	.call(yAxis)
+
+// Labels
+let xLabel = g.append('text')
+	.attr('x', plot.width / 2)
+	.attr('y', plot.height + 50)
+	.attr('text-anchor', 'middle')
+	.text('GDP Per Capita ($)')
+let yLabel = g.append('text')
+	.attr('x', -(plot.height / 2))
+	.attr('y', -50)
+	.attr('transform', 'rotate(-90)')
+	.attr('text-anchor', 'middle')
+	.text('Life Expectancy (Years)')
+let timeLabel = g.append("text")
+	.attr("y", plot.height - 10)
+	.attr("x", plot.width - 40)
+	.attr("font-size", "40px")
+	.attr("opacity", "0.4")
+	.attr("text-anchor", "middle")
+	.text("1800");
 
 d3.json("data/data.json").then(function(data){	
 	// Filter null values
@@ -54,14 +69,22 @@ d3.json("data/data.json").then(function(data){
 	// 	})
 	// })
 
-	// Grab first year in data set
-	let firstYear = data.shift()
-	let newData = firstYear.countries.filter(country => {
+	// Get data for first year (1800) in data set
+	// Year 1800, GDP: min = 350, max = 4235
+	// Year 1800, Life exp: min = 23.39, max = 42.85	
+	let firstYr = data.shift()
+	let firstYrData = firstYr.countries.filter(country => {
 		return country.income != null && country.life_exp != null
 	})
 
-	// Year 1800, GDP: min = 350, max = 4235
-	// Year 1800, Life exp: min = 23.39, max = 42.85
+	let circles = g.selectAll('circle')
+		.data(firstYrData, d => d.country)
+		.enter()
+		.append('circle')
+		.attr('fill', 'grey')
+		.attr('r', 5)
+		.attr('cx', d => x(d.income))
+		.attr('cy', d => y(d.life_exp))
 	
-	console.log(newData);
+	console.log(firstYrData);
 })
