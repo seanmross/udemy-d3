6647fce6a8becc63512d5109ce5a73bea8864429
Sean Ross <seanross917@gmail.com>
2020-02-04 07:46:27 -0500

    added fill colors and legend

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -30,6 +30,7 @@ let y = d3.scaleLinear()
 let area = d3.scaleLinear()
 	.domain([2000, 1400000000])
 	.range([25*Math.PI, 1500*Math.PI])
+let fill = d3.scaleOrdinal(d3.schemeSet2)
 
 // Axis
 let xAxis = d3.axisBottom(x)
@@ -66,6 +67,29 @@ let timeLabel = g.append("text")
 // Transition for updating elements
 const t = d3.transition().duration(100)
 
+// Legend
+const continents = ["africa", "americas", "asia", "europe"]
+let legend = g.append('g')
+	.attr('transform', `translate(${plot.width - 10}, ${plot.height - 125})`)
+
+continents.forEach( (continent, i) => {
+	let row = legend.append('g')
+		.attr('transform', `translate(0, ${i * 20})`)
+
+	row.append('rect')
+		.attr('height', 10)
+		.attr('width', 10)
+		.attr('fill', fill(continent))
+	
+	row.append('text')
+		.attr('x', -10)	
+		.attr('y', 10)
+		.attr('text-anchor', 'end')
+		.style('text-transform', 'capitalize')
+		.text(continent)
+})
+
+
 d3.json("data/data.json").then(function(data){	
 	// Filter null values
 	data.forEach( year => {
@@ -73,7 +97,7 @@ d3.json("data/data.json").then(function(data){
 			return country.income != null && country.life_exp != null
 		})
 	})
-
+	
 	let year = 0
 
 	d3.interval( () => {
@@ -100,11 +124,11 @@ function update(data) {
 	circles
 		.enter()
 		.append('circle')
-		.attr('fill', 'grey')
 		// AND UPDATE old elements present in new data
 		.merge(circles)
 		.transition(t)
-			.attr('r', d=> Math.sqrt(area(d.population) / Math.PI))
+			.attr('fill', d => fill(d.continent))
+			.attr('r', d => Math.sqrt(area(d.population) / Math.PI))
 			.attr('cx', d => x(d.income))
 			.attr('cy', d => y(d.life_exp))
 }
